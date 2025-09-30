6eed19737df0af71a8b0a525faf260649b26674d
roeehub <roeedar737@gmail.com>
2025-09-30 14:37:32 +0300

    wip

--- DeepfakeBench/training/wma/participant_manager.py
+++ DeepfakeBench/training/wma/participant_manager.py
@@ -17,8 +17,8 @@ import numpy as np
 import wma_streaming_pb2 as pb2
 
 # --- Configuration Constants ---
-HISTORY_WINDOW_SIZE = 96  # Total number of probabilities to keep per participant
-ACTIVE_TEST_WINDOW = 48  # Number of recent probabilities to use for verdict calculation
+HISTORY_WINDOW_SIZE = 48  # Total number of probabilities to keep per participant
+ACTIVE_TEST_WINDOW = 16  # Number of recent probabilities to use for verdict calculation
 DEFAULT_START_PROB = 0.2  # Assumed probability for a new, unseen participant
 MIN_RESPONSE_INTERVAL = 4  # Send a response every N batches even if verdict is stable
 RESET_AFTER_INACTIVE_MIN = 2.0  # Forget a participant after this many minutes of inactivity
